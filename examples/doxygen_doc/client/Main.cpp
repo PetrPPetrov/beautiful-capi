11f5c851084e9185649f580752bf033493115318
Sergey Rudenko <srudenko@gkmsoft.ru>
2017-05-16 11:42:34 +0300

    Added doxygen_doc example.

---
+++ examples/doxygen_doc/client/Main.cpp
@@ -0,0 +1,58 @@
+/*
+ * Beautiful Capi generates beautiful C API wrappers for your C++ classes
+ * Copyright (C) 2015 Petr Petrovich Petrov
+ *
+ * This file is part of Beautiful Capi.
+ *
+ * Beautiful Capi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beautiful Capi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Beautiful Capi.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#if defined(_WIN32) && defined(_DEBUG)
+#include <crtdbg.h>
+#endif
+#include <iostream>
+#include <cstdlib>
+#include "DoxygenDoc.h"
+
+int main()
+{
+#if defined(_WIN32) && defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    DoxygenDoc::Printer printer;
+
+    DoxygenDoc::Name name("Jon", "Snow");
+    printer.Show(name);
+    printer.ShowByPointer(name);
+    printer.ShowByReference(name);
+
+    DoxygenDoc::PersonRawPtr person;
+    person->SetName(name);
+    person->SetDay(27);
+    person->SetMonth(12);
+    person->SetYear(324);
+    printer.Show(person);
+    printer.ShowByPointer(person);
+    printer.ShowByReference(person);
+
+    DoxygenDoc::AddressPtr address;
+    address->SetStreetName("Wall");    
+    person->SetAddress(address);
+    printer.Show(person->GetAddress());
+    printer.ShowByPointer(person->GetAddress());
+    printer.ShowByReference(person->GetAddress());
+    return EXIT_SUCCESS;
+}
---
+++ examples/doxygen_doc/library/source/PrinterImpl.h
@@ -0,0 +1,82 @@
+/*
+ * Beautiful Capi generates beautiful C API wrappers for your C++ classes
+ * Copyright (C) 2015 Petr Petrovich Petrov
+ *
+ * This file is part of Beautiful Capi.
+ *
+ * Beautiful Capi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beautiful Capi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Beautiful Capi.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef BEAUTIFUL_CAPI_DOXYGEN_DOC_PRINTER_H
+#define BEAUTIFUL_CAPI_DOXYGEN_DOC_PRINTER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stddef.h>
+
+namespace DoxygenDoc
+{
+    class PrinterImpl
+    {
+    public:
+        void Show(DoxygenDoc::NameImpl name)
+        {
+            std::cout << "First Name: " << name.GetFirstName() << std::endl;
+        }
+
+         void ShowByPointer(const DoxygenDoc::NameImpl* name)
+        {
+            std::cout << "First Name: " << name->GetFirstName() << std::endl;
+        }
+
+        void ShowByReference(const DoxygenDoc::NameImpl& name)
+        {
+            std::cout << "Last Name: " << name.GetLastName() << std::endl;
+        }
+
+        void Show(DoxygenDoc::PersonImpl person)
+        {
+            std::cout << "Person birth day: " << person.GetDay() << std::endl;
+        }
+
+        void ShowByReference(const DoxygenDoc::PersonImpl& person)
+        {
+            std::cout << "Person birth month: " << person.GetMonth() << std::endl;
+        }
+
+        void ShowByPointer(DoxygenDoc::PersonImpl* person)
+        {
+            std::cout << "Person birth year: " << person->GetYear() << std::endl;
+        }
+
+        void Show(DoxygenDoc::AddressImpl addr)
+        {
+            std::cout << "Person street: " << addr.GetStreetName() << std::endl;
+        }
+
+        void ShowByPointer(DoxygenDoc::AddressImpl* addr)
+        {
+            std::cout << "Person city: " << addr->GetCity() << std::endl;
+        }
+
+        void ShowByReference(const DoxygenDoc::AddressImpl& addr)
+        {
+            std::cout << "Person state: " << addr.GetState() << std::endl;
+        }
+    };
+}
+
+#endif /* BEAUTIFUL_CAPI_DOXYGEN_DOC_PRINTER_H */
