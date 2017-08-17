5d31931ad75be998f6eec7db826a24977d23a54e
Petr Petrov <petr@gkmsoft.ru>
2017-08-17 09:25:39 +0300

    Schema documentation: don't generate empty line quotes
    Signed-off-by: Petr Petrov <petr@gkmsoft.ru>

--- source/Xsd2Markdown.py
+++ source/Xsd2Markdown.py
@@ -28,8 +28,8 @@ from FileGenerator import FileGenerator, Indent
 def get_attribute(node, attribute):
     if node.hasAttribute(attribute):
         attr = node.getAttribute(attribute)
-        return attr if attr else '""'
-    return '""'
+        return attr if attr else '*empty string*'
+    return '*empty string*'
 
 
 def get_children_by_tag(node, tag: str):
