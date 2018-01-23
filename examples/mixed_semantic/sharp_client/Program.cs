9b45438ee52a838f10c7e58217b7bb6e2809f25f
Sergey Rudenko <srudenko@gkmsoft.ru>
2018-01-23 09:02:30 +0300

    Added C#-version for hello_world, api_include, mixed_semantic examples

---
+++ examples/mixed_semantic/sharp_client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mixed_semantic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MixedSemantic.Printer printer = new MixedSemantic.Printer();
+
+            MixedSemantic.Name name = new MixedSemantic.Name("Jon", "Edward", "Snow");
+            printer.Show(name);
+            printer.ShowByPointer(name);
+            printer.ShowByReference(name);
+
+            MixedSemantic.PersonRawPtr person = new MixedSemantic.PersonRawPtr();
+            person.SetName(name);
+            person.SetDay(27);
+            person.SetMonth(12);
+            person.SetYear(324);
+            printer.Show(person);
+            printer.ShowByPointer(person);
+            printer.ShowByReference(person);
+
+            MixedSemantic.AddressPtr address = new MixedSemantic.AddressPtr();
+            address.SetStreetName("Wall");
+            person.SetAddress(address);
+            printer.Show(person.GetAddress());
+            printer.ShowByPointer(person.GetAddress());
+            printer.ShowByReference(person.GetAddress());
+
+
+            Console.WriteLine("Mutate \n");
+            MixedSemantic.Mutator mutator = new MixedSemantic.Mutator();
+            mutator.Mutate(name);
+            mutator.MutateByPointer(name);
+            mutator.MutateByReference(name);
+
+            mutator.Mutate(person);
+            mutator.MutateByPointer(person);
+            mutator.MutateByReference(person);
+
+            mutator.Mutate(person.GetAddress());
+            mutator.MutateByPointer(person.GetAddress());
+            mutator.MutateByReference(person.GetAddress());
+
+            printer.Show(name);
+            printer.ShowByPointer(name);
+            printer.ShowByReference(name);
+
+            printer.Show(person);
+            printer.ShowByPointer(person);
+            printer.ShowByReference(person);
+
+            printer.Show(person.GetAddress());
+            printer.ShowByPointer(person.GetAddress());
+            printer.ShowByReference(person.GetAddress());
+
+            Console.Write("Press any key to close programm...");
+            Console.ReadKey();
+
+        }
+    }
+}
