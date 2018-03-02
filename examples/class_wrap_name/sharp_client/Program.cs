0b035525bd1cc5f4b65814eeff0d927a326491d8
AlexKik <36221583+AlexKik@users.noreply.github.com>
2018-03-02 15:01:14 +0300

    C# class_wrap_name examples

---
+++ examples/class_wrap_name/sharp_client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new ClassWrapName.WrapPrinter();
+            printer.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
