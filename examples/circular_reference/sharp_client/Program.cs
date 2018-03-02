64fec4e9317b6e24ec5c97844ca20ff3f0f112c0
AlexKik <36221583+AlexKik@users.noreply.github.com>
2018-03-02 14:21:53 +0300

    Added C# circular_reference example.

---
+++ examples/circular_reference/sharp_client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a_object = new Circular.ClassARawPtr();
+            var b_object = new Circular.ClassBRawPtr();
+            var a_object1 = new Circular.ClassARawPtr();
+            var b_object1 = new Circular.ClassBRawPtr();
+
+            a_object.SetB(b_object);
+            b_object1.SetA(a_object1);
+
+            Circular.ClassARawPtr tmp_object = b_object1.GetA();
+            tmp_object.SetB(b_object);
+
+            a_object.GetB().SetA(a_object1);
+
+            a_object.Delete();
+            b_object.Delete();
+            a_object1.Delete();
+            b_object1.Delete();
+
+            Console.ReadKey();
+        }
+    }
+}
