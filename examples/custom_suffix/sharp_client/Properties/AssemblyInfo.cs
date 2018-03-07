0e1800a5a638360635030cb094f40d3a908de560
AlexKik <36221583+AlexKik@users.noreply.github.com>
2018-03-07 12:35:34 +0300

    C# copy_semantic and custom_suffix examples

---
+++ examples/custom_suffix/sharp_client/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("CSharpCustomSuffix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpCustomSuffix")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53639e11-fd96-42af-9c84-37ee2f1f318f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
