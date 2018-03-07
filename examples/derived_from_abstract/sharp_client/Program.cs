d8952443e3bf2119d87c3bc0ea55d84e1c0c06f0
AlexKik <36221583+AlexKik@users.noreply.github.com>
2018-03-07 13:55:57 +0300

    C# derived_from_abstarct example

---
+++ examples/derived_from_abstract/sharp_client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DerivedFromAbstract;
+
+namespace DerivedFormAbstractSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bird = new DerivedFromAbstract.Bird("Worm");
+            bird.Move();
+            bird.Sound();
+
+            var wolf = new DerivedFromAbstract.Wolf("Meat");
+            wolf.Move();
+            wolf.Sound();
+
+            Console.WriteLine("==============");
+            Console.WriteLine("Program ending");
+            Console.ReadKey();
+        }
+    }
+}
